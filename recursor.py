7ef9392fae2c4bd56f1dc07126eaa32b5d4c6efe
navin <navinipe@gmail.com>
2014-04-18 22:54:35 +0530

    corrected a bug

--- recursor.py
+++ recursor.py
@@ -1,23 +1,24 @@
 import os
 
-def folders():	
-	contents=os.listdir()
-	"print('Contents of this folder are')"
-	"print(contents)"
-	onlyfileshere = 1
-	
-	for i in contents:
-		if os.path.isfile(i):
-			if 'Thumbs.db' in i:
-				os.remove("Desktop.ini")
-		else:
-			onlyfileshere = 0
-			print('--------- entering directory: ' + i )
-			os.chdir(i)
-			folders()
-	
-	os.chdir('..')
-	return
+def folders():
+    contents=os.listdir()
+    "print('Contents of this folder are')"
+    "print(contents)"
+    onlyfileshere = 1
+
+    for i in contents:
+        if os.path.isfile(i):
+            if 'Desktop.ini' in i:
+                os.remove("Desktop.ini")
+                print('Deleted Desktop.ini')
+        else:
+            onlyfileshere = 0
+            print('--------- entering directory: ' + i )
+            os.chdir(i)
+            folders()
+
+    os.chdir('..')
+    return
 
 folders()
 
